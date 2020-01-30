1be86be7ad893b71af3856bfbb86e795ce8d0599
Nick801218 <nick757416@gmail.com>
2020-01-30 16:25:37 +0800

    Datatype

---
+++ datatype/datatype.py
@@ -0,0 +1,38 @@
+# Number
+3476
+24.3
+
+# String
+"哈囉"
+"hello world"
+
+# Boolean
+True
+False
+
+# List(ordered and changeable)
+[3,4,5]
+["hello","world"]
+
+# Tuple(ordered and unchangeable)
+(3,4,5)
+("hello","world")
+
+# Set(unordered and unindexed)
+{3,4,5}
+{"hello","world"}
+
+# Dictionary(unordered, changeable and indexed)
+{"apple":"蘋果",
+"data":"資料"}
+
+# Variable
+x=5
+print(x)
+x=True
+print(x)
+x=[1,2,3]
+print(x)
+x=(1,2,3)
+print(x)
+
