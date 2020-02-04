7e832d5171bef0691a50c2c16457068f417b7926
Nick801218 <nick757416@gmail.com>
2020-02-04 19:42:19 +0800

    Operators

--- Operators/operator.py
+++ Operators/operator.py
@@ -1,2 +1,27 @@
+x=3
+y=7
+# Division
+z=x/y
+print(z)
+# Floor division
+z=x//y
+print(z)
+# Modulus
+z=x%y
+print(z)
+# Exponentiation
+z=x**y
+print(z)
+# x=x+y
+x+=y
+print(x)
+
+# Bitwise Operators
+
+print(1&1)# AND
+print(1|0)# OR
+print(1^0)# XOR
+
+
 x=True & False
 print(x)
