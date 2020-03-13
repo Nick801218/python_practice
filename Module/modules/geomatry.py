7a009ec5c7510b19db69827bee022f7509b92f3b
Nick801218 <nick757416@gmail.com>
2020-03-13 16:27:05 +0800

    geo_module

---
+++ Module/modules/geomatry.py
@@ -0,0 +1,6 @@
+# Definite geomatry module
+def dis(x1,y1,x2,y2): # Caculate distance of two points
+    return ((x2-x1)**2+(y2-y1)**2)**0.5
+
+def slope(x1,y1,x2,y2): # Caculate slope of the line through two points
+    return (y2-y1)/(x2-x1)
