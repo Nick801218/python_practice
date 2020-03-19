c0c117116ded7a84f2d5aaaa9bacf85c788b73dc
Nick801218 <nick757416@gmail.com>
2020-03-19 17:00:25 +0800

    modify module path

--- Module_Package/module.py
+++ Module_Package/module.py
@@ -4,10 +4,10 @@ print(sys.platform)
 print(sys.maxsize)
 
 # Import the gematry module amd create an alias for module called geo
-sys.path.append("d:\\python\\python_practice\\Module\\modules") # Add new module path in system path
+sys.path.append("d:\\python\\python_practice\\Module_Package\\modules") # Add new module path in system path
 import geomatry as geo
 print(geo.dis(1,1,5,5))
 print(geo.slope(5,9,6,2))
 
 # Print the path of module
-print(sys.path)
+#print(sys.path)
