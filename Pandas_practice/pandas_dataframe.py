e56c1bb2169b0ab402e42c6b35a9013bcdfffb19
Nick_Wu <nick757416@gmail.com>
2023-07-05 15:01:06 +0800

    change code style

--- Pandas_practice/pandas_dataframe.py
+++ Pandas_practice/pandas_dataframe.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-data=pd.DataFrame({
-    "name":["Nick", "James", "Amy"],
-    "salary":[50000, 35000, 60000]
-},index=["A","B","C"])
+data = pd.DataFrame({
+    "name": ["Nick", "James", "Amy"],
+    "salary": [50000, 35000, 60000]
+}, index=["A", "B", "C"])
 print(data)
 print("==============")
-print("Numbers of elements:",data.size)
-print("Numbers of rows and columns:",data.shape)
-print("Data index:",data.index)
+print("Numbers of elements:", data.size)
+print("Numbers of rows and columns:", data.shape)
+print("Data index:", data.index)
 print("==============")
 print("Get second row data by order:", data.iloc[1], sep="\n")
 print("Get second row data by index:", data.loc["B"], sep="\n")
 print("==============")
 print("Get the column called \"name\":", data["name"], sep="\n")
 print("==============")
-data["revenue"]=[300000,400000,500000]
-data["rank"]=pd.Series([3,2,1],index=["A","B","C"])
-data["CP"]=data["revenue"]/data["salary"]
+data["revenue"] = [300000, 400000, 500000]
+data["rank"] = pd.Series([3, 2, 1], index=["A", "B", "C"])
+data["CP"] = data["revenue"] / data["salary"]
 print(data)
--- Random_Statistic/random_statistic.py
+++ Random_Statistic/random_statistic.py
@@ -1,24 +1,25 @@
 # Random module (Do not name your file the same name as the module)
 import random
-data_ch=random.choice([1,6,3,7,2])
+
+data_ch = random.choice([1, 6, 3, 7, 2])
 print(data_ch)
 
-data_sam=random.sample([1,6,3,7,2],3)
+data_sam = random.sample([1, 6, 3, 7, 2], 3)
 print(data_sam)
 
-data_shl=[1,70,3,7]
+data_shl = [1, 70, 3, 7]
 random.shuffle(data_shl)
 print(data_shl)
 
-data_uni=random.random()# =random.unform(0.0,1.0)
+data_uni = random.random()  # =random.unform(0.0,1.0)
 print(data_uni)
 
-data_nor=random.normalvariate(0,10)
+data_nor = random.normalvariate(0, 10)
 print(data_nor)
 
 # Statistic module
 import statistics as stat
-print(stat.mean([1,7,36,80]))
-print(stat.median([1,7,36,80]))
-print(stat.stdev([1,7,36,80]))
 
+print(stat.mean([1, 7, 36, 80]))
+print(stat.median([1, 7, 36, 80]))
+print(stat.stdev([1, 7, 36, 80]))
